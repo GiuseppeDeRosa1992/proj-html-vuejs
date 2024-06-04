e6990c7979292c7cd1e2bd19a5df63396713dc74
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-06-04 13:07:07 +0200

    header da completare nella sezione shop e contacts

--- src/data/store.js
+++ src/data/store.js
@@ -1,21 +1,80 @@
 import { reactive } from 'vue';
 
 const iMieiDati = reactive({
-    links: [
-        "Home",
-        "Celebrity",
-        "Movie",
-        "Page",
-        "Shop",
-        "Contacts",
+    headerLinks: [
+        {
+            titolo: "Home",
+            link: [
+                "Home",
+                "Home1",
+                "Home2",
+                "Home3",
+                "Home4",
+                "Home5",
+                "Home6",
+            ]
+        },
+        {
+            titolo: "Celebrity",
+            link: [
+                "Celebrity Single",
+                "Celebrity",
+            ]
+        },
+        {
+            titolo: "Movie",
+            link: [
+                "Movie Post",
+                "Top Rate Movie",
+                "Movie Category",
+                "Single Movie",
+            ]
+        },
+        {
+            titolo: "Page",
+            link: [
+                "Princing Table",
+                "Call to Action",
+                "My Account",
+                "Cart",
+                "Checkout",
+                "Wishlist",
+            ]
+        },
+        {
+            titolo: "Shop",
+            link: [
+                {
+                    titolo: "Shop Sidebar",
+                    link: [
+                        "Grid View",
+                        "List View"
+                    ],
+                },
+
+                {
+                    titolo: "No Sidebar",
+                    link: [
+                        "Grid View",
+                        "List View"
+                    ],
+                }
+
+            ]
+        },
+        {
+            titolo: "Contacts",
+            link: null
+        },
+
     ],
-    linksHome: [
-        "Home",
-        "Home1",
-        "Home2",
-        "Home3",
-        "Home4",
-        "Home5",
+
+    searchLinks: [
+        "All Movie Category",
+        "Coming Soon",
+        "Latest Movie",
+        "Top Rating",
+        "Tv Series",
     ]
 });
 
