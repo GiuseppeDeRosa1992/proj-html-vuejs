18f3bd33ffa398db4554aa31e803d1d670c1ba29
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-06-04 18:25:27 +0200

    added JumboComing.vue with countdown

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import Jumbo from './components/Jumbo.vue';
+import JumboComing from './components/JumboComing.vue'
 import store from './data/store.js';
 export default {
   components: {
     AppHeader,
     Jumbo,
+    JumboComing,
   },
 
   data() {
@@ -25,6 +27,7 @@ export default {
 <template>
   <AppHeader />
   <Jumbo />
+  <JumboComing />
 </template>
 
 <style scoped></style>
